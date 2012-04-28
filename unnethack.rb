b6c6110d2aa606c0e224d439da8326f358677cc7
Patric Mueller <bhaak@gmx.net>
2012-04-28 20:58:35 +0200

    unnethack 4.0.0
    Added head for svn repository.
    Bones and save directories moved into #{var}
    Disable compile optimization because of crash reports
    
    Closes #6.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- unnethack.rb
+++ unnethack.rb
@@ -2,16 +2,28 @@ require 'formula'
 
 class Unnethack < Formula
   homepage 'http://sourceforge.net/apps/trac/unnethack/'
-  url 'https://downloads.sourceforge.net/project/unnethack/unnethack/3.6.1/unnethack-3.6.1-20120205.tar.gz'
-  md5 '675c00d776e513d5aedaf11f071af569'
+  url 'https://downloads.sourceforge.net/project/unnethack/unnethack/4.0.0/unnethack-4.0.0-20120401.tar.gz'
+  sha1 'c7415284b27a585024b56858c87e927b36bc6ad5'
+  head 'http://unnethack.svn.sourceforge.net/svnroot/unnethack/trunk'
 
-  skip_clean "var/unnethack/bones"
+  # directory for temporary level data of running games
   skip_clean "var/unnethack/level"
-  skip_clean "var/unnethack/saves"
 
   def install
+    # crashes when using clang and gsl with optimizations
+    # https://github.com/mxcl/homebrew/pull/8035#issuecomment-3923558
+    ENV.no_optimization
+
+    # directory for version specific files that shouldn't be deleted when
+    # upgrading/uninstalling
+    version_specific_directory = "#{var}/unnethack/#{version}"
+
     system "./configure", "--prefix=#{prefix}",
                           "--with-owner=#{`id -un`}", "--with-group=admin",
+                          # common xlogfile for all versions
+                          "--enable-xlogfile=#{var}/unnethack/xlogfile",
+                          "--with-bonesdir=#{version_specific_directory}/bones",
+                          "--with-savesdir=#{version_specific_directory}/saves",
                           "--enable-wizmode=#{`id -un`}"
     system "make install"
   end
