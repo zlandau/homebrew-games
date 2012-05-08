3fe3f815d2d58be01160a0d5d6f54bb869557a97
Jack Nagel <jacknagel@gmail.com>
2012-05-08 00:11:38 -0500

    openttd 1.2.0
     - use subformulae for extra package downloads
     - update opengfx to 0.4.4
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- openttd.rb
+++ openttd.rb
@@ -1,36 +1,41 @@
 require 'formula'
 
+class Opengfx < Formula
+  url 'http://bundles.openttdcoop.org/opengfx/releases/0.4.4/opengfx-0.4.4.zip'
+  sha1 'c68cdbb1ee8980a44feff320288e466d06efcd19'
+end
+
+class Opensfx < Formula
+  url 'http://bundles.openttdcoop.org/opensfx/releases/0.2.3/opensfx-0.2.3.zip'
+  sha1 'bfbfeddb91ff32a58a68488382636f38125c48f4'
+end
+
+class Openmsx < Formula
+  url 'http://bundles.openttdcoop.org/openmsx/releases/0.3.1/openmsx-0.3.1.zip'
+  sha1 'e9c4203923bb9c974ac67886bd00b7090658b961'
+end
+
 class Openttd < Formula
   homepage 'http://www.openttd.org/'
-  url 'http://us.binaries.openttd.org/binaries/releases/1.1.5/openttd-1.1.5-source.tar.gz'
-  md5 '6bad4750c09782e04a987a326d798d8a'
+  url 'http://us.binaries.openttd.org/binaries/releases/1.2.0/openttd-1.2.0-source.tar.gz'
+  sha1 '6094f37c27fd8801c23c588b9945d14919b53132'
+
   head 'git://git.openttd.org/openttd/trunk.git'
 
   depends_on 'lzo'
   depends_on 'xz'
 
+  fails_with :clang do
+    build 318
+  end
+
   def install
     system "./configure", "--prefix-dir=#{prefix}"
     system "make bundle"
 
-    def install_asset(url)
-      ohai "Downloading asset: #{url}"
-      curl url, '-O'
-
-      file = File.basename url
-      system '/usr/bin/unzip', '-qq', file
-      rm file
-    end
-
-    cd 'bundle/OpenTTD.app/Contents/Resources' do
-      cd 'data' do
-        install_asset 'http://bundles.openttdcoop.org/opengfx/releases/0.3.3/opengfx-0.3.3.zip'
-        install_asset 'http://bundles.openttdcoop.org/opensfx/releases/0.2.3/opensfx-0.2.3.zip'
-      end
-      cd 'gm' do
-        install_asset 'http://bundles.openttdcoop.org/openmsx/releases/0.3.1/openmsx-0.3.1.zip'
-      end
-    end
+    Opengfx.new.brew { (buildpath/'bundle/OpenTTD.app/Contents/Resources/data/opengfx').install Dir['*'] }
+    Opensfx.new.brew { (buildpath/'bundle/OpenTTD.app/Contents/Resources/data/opensfx').install Dir['*'] }
+    Openmsx.new.brew { (buildpath/'bundle/OpenTTD.app/Contents/Resources/gm/openmsx').install Dir['*'] }
 
     prefix.install 'bundle/OpenTTD.app'
   end
