0c55127f9419ab9930411d9681812158c60902c4
Justin de Vesine <justin@crowdfavorite.com>
2012-07-20 11:14:07 -0600

    frobtads 1.2
    Closes #11.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- frobtads.rb
+++ frobtads.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Frobtads < Formula
   homepage 'http://www.tads.org/frobtads.htm'
-  url 'http://www.tads.org/frobtads/frobtads-1.1.tar.gz'
-  md5 '2d29d1449bac6f23b6c5f522209069a1'
+  url 'http://www.tads.org/frobtads/frobtads-1.2.tar.gz'
+  sha1 '986e3a886ec6f2df73c298f51f9ed36153a68c2f'
   head 'git://git.assembla.com/frobtads.git'
 
   def install
