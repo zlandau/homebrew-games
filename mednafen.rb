90e5c772e4c1f6a295c6e2784a2c85c754036e03
Caimi Marco <mcaimi.ing@gmail.com>
2012-08-20 23:25:06 +0200

    mednafen 0.9.24-WIP
    This replaces the stable version of mednafen with the current WIP,
    which is what most users are interested in.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- mednafen.rb
+++ mednafen.rb
@@ -2,50 +2,22 @@ require 'formula'
 
 class Mednafen < Formula
   homepage 'http://mednafen.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/mednafen/Mednafen/0.8.D.3/mednafen-0.8.D.3.tar.bz2'
-  md5 '57d22805071becd81858b0c088a275e5'
-  version '0.8.D.3'
+  url 'http://forum.fobby.net/index.php?t=getfile&id=380'
+  sha1 'bc0d867a3fa255f01d04ee6f028acab18849f16d'
+  version '0.9.24-WIP'
 
-  devel do
-    url 'http://forum.fobby.net/index.php?t=getfile&id=358'
-    md5 '40516f81b18df70ae4a2dfd411fa0861'
-    version '0.9.21-WIP'
-  end
-
+  # as of xcode 4.4, building with clang still fails
   fails_with :clang do
-    build 318
+    build 421
   end
 
   depends_on 'pkg-config' => :build
   depends_on 'sdl'
-  depends_on 'sdl_net'
   depends_on 'libcdio'
   depends_on 'libsndfile'
 
-  def options
-    [
-      ["--with-psx", "Build experimental PlayStation emulator"]
-    ]
-  end
-
   def install
-    # Compiler produces code which fails math tests
-    # with optimizations enabled
-    # http://forum.fobby.net/index.php?t=msg&&th=659&goto=2254#msg_2254
-    ENV.no_optimization
-
-    args = [ "--disable-dependency-tracking", "--prefix=#{prefix}" ]
-
-    if ARGV.include? "--with-psx" and not ARGV.build_devel?
-      onoe "--with-psx is only supported with --devel"
-    end
-    args << "--enable-psx" if ARGV.include? "--with-psx" and ARGV.build_devel?
-
-    # Platform detection is buggy; problem reported upstream:
-    # http://forum.fobby.net/index.php?t=msg&&th=659&goto=2214#msg_2214
-    args << "--build=x86_64-apple-darwin#{`uname -r`}" if MacOS.prefer_64_bit?
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
 end
