d26f40567d63d2240747e1c6c99964d10f55aa12
Antoine Lyset <antoinelyset+github@gmail.com>
2012-08-21 23:32:01 +0200

    dwarf-fortress 0.34.11
    Closes #20.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- dwarf-fortress.rb
+++ dwarf-fortress.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class DwarfFortress < Formula
   homepage 'http://www.bay12games.com/dwarves/'
-  url 'http://www.bay12games.com/dwarves/df_34_09_osx.tar.bz2'
-  version '0.34.09'
-  md5 '0bc24891af8426cfd56c59a04fcee407'
+  url 'http://www.bay12games.com/dwarves/df_34_11_osx.tar.bz2'
+  version '0.34.11'
+  md5 'e1332943cf346c700c28cc1e90d24b1d'
 
   def install
     (bin+'dwarffortress').write <<-EOS.undent
