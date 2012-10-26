b5fd02885f9ddfb4bdf1fbb384b23704a50c93c4
David Baumgold <david@davidbaumgold.com>
2012-10-26 17:55:32 -0400

    roll 2.0.1
    Closes #31.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ roll.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Roll < Formula
+  homepage 'http://matteocorti.ch/software/roll.html'
+  url 'http://matteocorti.ch/software/roll/roll-2.0.1.tar.gz'
+  sha1 'a05174453e06bfbf250788c7e78dc44b29d3e6a6'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
