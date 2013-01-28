dba373fcb725e7b536cf3cf7e9ed2ddd6f70fc4b
Adam Vandenberg <flangy@gmail.com>
2013-01-28 12:52:54 -0800

    go-gui 1.4.6
    Fixes #15.

--- go-gui.rb
+++ go-gui.rb
@@ -2,10 +2,11 @@ require 'formula'
 
 class GoGui < Formula
   homepage 'http://gogui.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/gogui/gogui/1.4.2/gogui-1.4.2.zip'
-  md5 '0f5e95deff548699c368b71e088bea58'
+  url 'http://downloads.sourceforge.net/project/gogui/gogui/1.4.6/gogui-1.4.6.zip'
+  sha1 'a18cb93ee5268ce1f9e79d887538eb7e644ae0c1'
 
   def install
+    inreplace "build.xml", "/Developer/Tools/SetFile", "/usr/bin/SetFile"
     system "ant", "gogui.app", "-Ddoc-uptodate=true"
     prefix.install 'build/GoGui.app'
   end
@@ -13,7 +14,7 @@ class GoGui < Formula
   def caveats; <<-EOS.undent
     GoGui.app installed to:
       #{prefix}
-    Use \"brew linkapps\" to symlink into ~/Applications.
+    Use `brew linkapps` to symlink into ~/Applications.
     EOS
   end
 end
