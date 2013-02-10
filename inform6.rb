620ec846d4425c0dd0a28133e26f5addbafc03b9
Adam Vandenberg <flangy@gmail.com>
2013-02-10 10:43:29 -0800

    inform6: remove unused configure switch

--- inform6.rb
+++ inform6.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Inform6 < Formula
-  url 'http://ifarchive.flavorplex.com/if-archive/infocom/compilers/inform6/source/inform-6.32.1.tar.gz'
   homepage 'http://www.inform-fiction.org/inform6.html'
+  url 'http://ifarchive.flavorplex.com/if-archive/infocom/compilers/inform6/source/inform-6.32.1.tar.gz'
   sha1 '251cf057ddbf750a286802d2cfbccf5e80e25295'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
   end
