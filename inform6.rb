2814816c3ecb06dc96d39efa2c78bf3aeb169c76
Justin de Vesine <ziz@ziz.org>
2013-04-03 14:41:58 -0600

    inform6: add HEAD build, fix overwritten i6 libs
    Fixes stable's build process overwriting the inform6 library
    files with differently-cased symlinks.
    
    HEAD is not affected, since upstream has fixed this:
    https://github.com/DavidGriffith/inform6unix/pull/1
    
    Closes #46.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- inform6.rb
+++ inform6.rb
@@ -4,8 +4,24 @@ class Inform6 < Formula
   homepage 'http://www.inform-fiction.org/inform6.html'
   url 'http://ifarchive.flavorplex.com/if-archive/infocom/compilers/inform6/source/inform-6.32.1.tar.gz'
   sha1 '251cf057ddbf750a286802d2cfbccf5e80e25295'
+  head 'https://github.com/DavidGriffith/inform6unix.git'
+
+  if build.head?
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
+
+  def patches
+    unless build.head?
+      # Fixes case-insensitivity issue that removes critical inform6 library files
+      # https://github.com/DavidGriffith/inform6unix/pull/1 (fixed in HEAD)
+      "https://gist.github.com/ziz/f2c0554ab0fefffab54b/raw/inform6unix-6.32.1.patch"
+    end
+  end
 
   def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
