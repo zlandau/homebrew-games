5159c786395729ccc59667c2a2bc80c331aa243f
Liang Qi <cavendish.qi@gmail.com>
2013-05-06 20:45:09 +0200

    xboard 4.7.1
    Closes #54.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- xboard.rb
+++ xboard.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Xboard < Formula
-  url 'http://ftpmirror.gnu.org/xboard/xboard-4.7.0.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/xboard/xboard-4.7.0.tar.gz'
+  url 'http://ftpmirror.gnu.org/xboard/xboard-4.7.1.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/xboard/xboard-4.7.1.tar.gz'
   homepage 'http://www.gnu.org/software/xboard/'
-  sha1 '814597b4f7845a582e63f01c8c6517ad42dac7c4'
+  sha1 '0f23e01561fedb1c05a72734f37fd3fce5a7c5aa'
 
   depends_on 'pkg-config' => :build
   depends_on 'fairymax' => :recommended
