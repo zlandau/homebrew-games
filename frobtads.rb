66710c7c63985683915c56d405f21dbd8f8a5d45
Justin de Vesine <ziz@ziz.org>
2013-05-21 14:20:57 -0600

    frobtads 1.2.3
    Closes #55.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- frobtads.rb
+++ frobtads.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Frobtads < Formula
   homepage 'http://www.tads.org/frobtads.htm'
-  url 'http://www.tads.org/frobtads/frobtads-1.2.2.tar.gz'
-  sha1 'd3e66d17a36f2084b878cfa752d337ded013211b'
+  url 'http://www.tads.org/frobtads/frobtads-1.2.3.tar.gz'
+  sha1 '330217c0b6ee298bf9db986bc4ce84b86aa5e3e2'
   head 'git://git.assembla.com/frobtads.git'
 
   if build.head?
