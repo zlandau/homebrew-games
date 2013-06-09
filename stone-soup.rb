b64535fc843f2d73647fb997235da5f81ab2ff40
Vitaly Lischenko <lischenko@gmail.com>
2013-06-09 23:15:35 +0400

    stone-soup 0.12.2
    Just learned that a newer stable version is available
    
    Closes #59.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- stone-soup.rb
+++ stone-soup.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class StoneSoup < Formula
   homepage 'http://crawl.develz.org/'
-  url 'http://sourceforge.net/projects/crawl-ref/files/Stone%20Soup/0.12.1/stone_soup-0.12.1.tar.xz'
-  sha1 '1c627b970711f9e8639835720fd7b01654777e12'
+  url 'http://sourceforge.net/projects/crawl-ref/files/Stone%20Soup/0.12.2/stone_soup-0.12.2.tar.xz'
+  sha1 'e70e7274896f9c78620f91d12bb85010c051766b'
 
   depends_on 'xz' => :build
 
