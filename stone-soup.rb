ef4fc7395087fe5754a4c81298525d262ae44dbe
Vitaly Lischenko <lischenko@gmail.com>
2013-06-09 22:23:33 +0400

    stone-soup 0.12.1
    Closes #58.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- stone-soup.rb
+++ stone-soup.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class StoneSoup < Formula
   homepage 'http://crawl.develz.org/'
-  url 'http://sourceforge.net/projects/crawl-ref/files/Stone%20Soup/0.11.0/stone_soup-0.11.0.tar.xz'
-  sha1 '5a4674b0ee032040d49c5f23a2f215b957b06440'
+  url 'http://sourceforge.net/projects/crawl-ref/files/Stone%20Soup/0.12.1/stone_soup-0.12.1.tar.xz'
+  sha1 '1c627b970711f9e8639835720fd7b01654777e12'
 
   depends_on 'xz' => :build
 
