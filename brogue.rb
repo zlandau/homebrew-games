136448ea25da55026ebe66258da3eb69e43c83d7
Baptiste Fontaine <batifon@yahoo.fr>
2013-10-07 17:13:42 +0200

    brogue 1.7.3
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- brogue.rb
+++ brogue.rb
@@ -5,24 +5,26 @@ class Brogue < Formula
 
   # The OS X version doesn't contain a Makefile, so we
   # need to download the Linux version
-  url 'https://sites.google.com/site/broguegame/brogue-linux-1.7.2.tar.gz'
-  sha1 '31bde0c5922cca803d4ce4b7fa916247ad444401'
+  url 'https://sites.google.com/site/broguegame/brogue-linux-1.7.3.tar.gz'
+  sha1 'acd9d3f107518cbbcd8ad974fa6c261d6439fb8d'
 
   def install
-    doc.install 'Readme.rtf' => 'README.rtf'
-    doc.install 'agpl.txt' => 'COPYING'
+    cd 'brogue-1.7.3' do
+      doc.install 'Readme.rtf' => 'README.rtf'
+      doc.install 'agpl.txt' => 'COPYING'
 
-    system "make", "clean"
-    system "make", "curses"
+      system "make", "clean"
+      system "make", "curses"
 
-    # The files are installed in libexec
-    # and the provided `brogue` shell script,
-    # which is just a convenient way to launch the game,
-    # is placed in the `bin` directory.
-    inreplace 'brogue', /`dirname \$0`\/bin$/, libexec
-    bin.install 'brogue'
-    libexec.install 'bin/brogue'
-    libexec.install 'bin/keymap'
+      # The files are installed in libexec
+      # and the provided `brogue` shell script,
+      # which is just a convenient way to launch the game,
+      # is placed in the `bin` directory.
+      inreplace 'brogue', /`dirname \$0`\/bin$/, libexec
+      bin.install 'brogue'
+      libexec.install 'bin/brogue'
+      libexec.install 'bin/keymap'
+    end
   end
 
   def test
