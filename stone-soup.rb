0261bb3482c6df874d9c4bd49bbe47b9a10577a5
Alex Bukin <abukin@csu.fullerton.edu>
2013-10-10 20:16:40 -0700

    stone-soup 0.12.3
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- stone-soup.rb
+++ stone-soup.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class StoneSoup < Formula
   homepage 'http://crawl.develz.org/'
-  url 'http://sourceforge.net/projects/crawl-ref/files/Stone%20Soup/0.12.2/stone_soup-0.12.2.tar.xz'
-  sha1 'e70e7274896f9c78620f91d12bb85010c051766b'
+  url 'http://sourceforge.net/projects/crawl-ref/files/Stone%20Soup/0.12.3/stone_soup-0.12.3.tar.xz'
+  sha1 'ef8d3d9dec9e4866acee8f04880c6466e1bf5099'
 
   depends_on 'xz' => :build
 
