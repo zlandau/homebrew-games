0a7b057ec9082a93d2532d6da07ccfb2df6420c3
Zachary P. Landau <zlandau@jellofund.net>
2013-12-09 19:37:27 -0800

    wesnoth: add devel 1.11.7
    Works around #80.
    
    Signed-off-by: Zachary P. Landau <zlandau@jellofund.net>

--- wesnoth.rb
+++ wesnoth.rb
@@ -5,6 +5,11 @@ class Wesnoth < Formula
   url 'http://sourceforge.net/projects/wesnoth/files/wesnoth-1.10/wesnoth-1.10.6/wesnoth-1.10.6.tar.bz2'
   sha1 'c812a55a4544b894691f37ab95cc849d015a3041'
 
+  devel do
+    url 'http://downloads.sourceforge.net/project/wesnoth/wesnoth/wesnoth-1.11.7/wesnoth-1.11.7.tar.bz2'
+    sha1 'cbbf5b073896429da754634ee2c1e4fc69159e9d'
+  end
+
   depends_on 'cmake' => :build
   depends_on 'gettext'
   depends_on 'pango'
